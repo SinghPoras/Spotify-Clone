2a15c0a2a8c8951dc009b52d875acec1b3bd3959
PorasIITJ <b22ci029@iitj.ac.in>
2024-07-10 21:25:47 +0530

    Added more content to the page
    Just worked on HTML and CSS

---
+++ script.js
@@ -0,0 +1,14 @@
+// let cardList = document.querySelectorAll(".card")
+// let cardArray = [].slice.call(cardList);
+// cardArray.forEach(element => {
+//     console.log(element)
+//     let button = element.querySelector(".playButtonAlbum")
+//     element.addEventListener("mousemove", function(){
+//         button.classList.add('buttonHover')
+//         console.log(button.classList)
+//     })
+//     element.addEventListener("mouseout",function(){
+//         button.classList.remove('buttonHover')
+//         console.log(button.classList)
+//     })
+// });
